2f7f5726ae6ca45b1a0ffa9138e34ef20e7abab4
Yasser Messahli <100447585+yassermessahli@users.noreply.github.com>
2025-04-23 22:59:31 +0100

    [fix]: fix dataode not running
    the problem wa sin hdfs-site.xml where the env vars was treated as strings

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@
 echo "Starting SSH..."
 /usr/sbin/sshd
 
-
-
-# ensure Hadoop/JAVA env vars are seen by remote SSH sessions
+# ensure ENV VARS are seen by remote SSH sessions
 cat <<EOF >> /root/.bashrc
 export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk \
 export HADOOP_HOME=/opt/hadoop \
@@ -19,26 +17,12 @@ export HADOOP_OPTS="-Djava.library.path=/opt/hadoop/lib/native" \
 export PATH=$PATH:/usr/lib/jvm/jre-1.8.0-openjdk/bin:/opt/hadoop/bin:/opt/hadoop/sbin
 EOF
 
-echo "Starting NameNode..."
-
-# Ensure the logs directory exists
-mkdir -p /opt/hadoop/logs
-
-# Format the DataNode directory
-${HADOOP_HOME}/bin/hdfs datanode -format -force
-
-# Redirect NameNode logs to a file
-${HADOOP_HOME}/bin/hdfs namenode >> /opt/hadoop/logs/namenode.log 2>&1 &
-${HADOOP_HOME}/bin/hdfs datanode
-
 echo "Starting HDFS & YARN..."
 ${HADOOP_HOME}/sbin/start-dfs.sh
 ${HADOOP_HOME}/sbin/start-yarn.sh
 
-# fix ownership of the Hadoop logs directory
-chown -R root:root /opt/hadoop/logs
 
-echo "Hadoop started successfully!"
+echo "$(date): Hadoop started successfully on Cent OS instance!"
 # Keep container alive or provide interactive shell
 if [ $# -gt 0 ]; then
   exec "$@"
